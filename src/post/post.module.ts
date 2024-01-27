c51ae2d606bd211f4e71353ff6029e3608509a7e
turbosheep44 <matthew.axisa4@gmail.com>
2024-01-27 10:55:35 +0000

    auto organized imports

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { PostService } from './post.service';
 
 @Module({
   providers: [PostService],
