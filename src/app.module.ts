3856b55db284d273c941a421c456f4bbcfba3f57
turbosheep44 <matthew.axisa4@gmail.com>
2024-01-27 20:00:00 +0000

    get DB config from environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/blog', {
-      auth: { username: 'root', password: 'example' },
-      authSource: 'admin',
-    }),
+    MongooseModule.forRoot(
+      process.env.DB_URL ?? 'mongodb://localhost:27017/blog',
+      {
+        auth: {
+          username: process.env.DB_USER ?? 'root',
+          password: process.env.DB_PASS ?? 'example',
+        },
+        authSource: 'admin',
+      },
+    ),
     PostModule,
   ],
   controllers: [],
