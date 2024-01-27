cae6e9db2e2cacba9eefce79fbce5d507250afcb
turbosheep44 <matthew.axisa4@gmail.com>
2024-01-27 11:04:30 +0000

    added mongoose root module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/blog'),
+    PostModule,
+  ],
   controllers: [],
   providers: [],
 })
